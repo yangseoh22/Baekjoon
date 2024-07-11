6643d73fe900dedb054c3db3155ad87934dedae8
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2024-07-11 16:26:54 +0900

    [Bronze I] Title: 최소공배수, Time: 632 ms, Memory: 23124 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -8,18 +8,20 @@ class Main {
     	int[] lcms = new int[n];  //최소공배수 저장 배열
     	
     	for(int i=0; i<n; i++) {
-    		int x = sc.nextInt();  //사용자에게 두 숫자 입력 받기
-    		int y = sc.nextInt(); 
+    		int a = sc.nextInt();  //사용자에게 두 숫자 입력 받기
+    		int b = sc.nextInt(); 
     		
-    		int max = (x>y)? x : y ;
-    		int min = (x<y)? x : y ;
-    	
-    		for(int j=max; j<=max*min; j+=max) {  //최소 공배수 구하기
-    			if(j%x==0 && j%y==0) {
-    				lcms[i] = j;
-    				break;
-    			}
-    		}
+    		int max = (a>b)? a : b ;
+    	    int tmp = max;
+    		while (true) {
+                //두 수가 모두 나누어 떨어지는 경우
+                if(max%a == 0 && max%b == 0) {
+                    lcms[i] = max; //해당 수가 최소공배수
+                    break;
+                }
+                max += tmp;  //max값 만큼 더해 배수 계산
+            }
+            
     	}
     	
     	for(int l : lcms) {  //배열 모든 값 출력
