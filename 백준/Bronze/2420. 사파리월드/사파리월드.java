366719adfdac0535f02a7ad6ef414bf92c74c6f2
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2024-07-12 21:59:03 +0900

    [Bronze V] Title: 사파리월드, Time: 100 ms, Memory: 14044 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.abs(a - b));
+
+    }
+}
