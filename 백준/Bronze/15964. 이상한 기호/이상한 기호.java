e8be7273699adf93a0d9ffe18e5b515b6ae69d7c
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2024-07-18 14:23:27 +0900

    [Bronze V] Title: 이상한 기호, Time: 124 ms, Memory: 14176 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -7,8 +7,8 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        long a = Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
 
         System.out.println((a+b)*(a-b));
 
