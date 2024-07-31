1c5b77ee2afc8297db52db9fa37ccbd1186a1be7
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2024-08-01 02:04:21 +0900

    [Silver III] Title: 여우는 어떻게 울지?, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/9536. 여우는 어떻게 울지？/여우는 어떻게 울지？.java
+++ 백준/Silver/9536. 여우는 어떻게 울지？/여우는 어떻게 울지？.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,24 +13,30 @@ public class Main {
         for(int i = 0; i < t; i++) {
             String record = br.readLine();  //녹음된 소리
 
-            StringTokenizer st = new StringTokenizer(record);  // 녹음된 소리를 단어별로 분리
+            // 녹음된 소리를 단어별로 분리
+            StringTokenizer st = new StringTokenizer(record);
 
-            // 각 울음소리를 저장
             ArrayList<String> crying = new ArrayList<String>();
             while (st.hasMoreTokens()) {
+                // 각 울음소리를 저장
                 crying.add(st.nextToken());
             }
 
+            // 여우 제외의 동물들의 울음 소리를 입력 받고 제외
             while(true){
                 String str = br.readLine();
                 if (str.equals("what does the fox say?")) break;  // 종료 문장이면 종료
-                else{
-                    st = new StringTokenizer(str);
-                    st.nextToken(); st.nextToken();  // <동물> goes
+                st = new StringTokenizer(str);
+                st.nextToken(); st.nextToken();  // <동물> goes 다음이 <소리>
 
-                    // 다른 동물의 울음소리와 같을 경우 연결리스트에서 삭제
-                    String sound = st.nextToken();
-                    crying.removeIf(c -> c.equals(sound));
+                String sound = st.nextToken();  //다른 동물의 울음소리
+
+                // 다른 동물의 울음소리와 같을 경우 리스트에서 제거
+                for (int j = 0; j < crying.size(); j++) {
+                    if (crying.get(j).equals(sound)) {
+                        crying.remove(j);
+                        j--;  // 리스트 크기가 줄어들었으므로 인덱스를 조정
+                    }
                 }
             }
 
