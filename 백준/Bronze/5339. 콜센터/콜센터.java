3fd28258bc131505d3b3c9c3d074481683779294
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2024-08-14 01:24:48 +0900

    [Bronze V] Title: 콜센터, Time: 116 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |";
+        System.out.println(str);
+    }
+}
