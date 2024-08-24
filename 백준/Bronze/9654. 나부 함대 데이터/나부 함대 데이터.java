3b467d6d04afeced8067884b55edea7c4c0e1838
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2024-08-25 01:28:30 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 96 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println(String.format("%-15s%-15s%-11s%-10s", "SHIP NAME", "CLASS", "DEPLOYMENT", "IN SERVICE"));
+        System.out.println(String.format("%-15s%-15s%-11s%-10s", "N2 Bomber", "Heavy Fighter", "Limited", "21"));
+        System.out.println(String.format("%-15s%-15s%-11s%-10s", "J-Type 327 ", "Light Combat", "Unlimited", "1"));
+        System.out.println(String.format("%-15s%-15s%-11s%-10s", "NX Cruiser", "Medium Fighter", "Limited", "18"));
+        System.out.println(String.format("%-15s%-15s%-11s%-10s", "N1 Starfighter", "Medium Fighter", "Unlimited", "25"));
+        System.out.println(String.format("%-15s%-15s%-11s%-10s", "Royal Cruiser", "Light Combat", "Limited", "4"));
+    }
+}
