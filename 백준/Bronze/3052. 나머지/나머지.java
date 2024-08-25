b09042a7ec150bbc1292973db43a759bea7f867f
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2024-08-26 01:48:18 +0900

    [Bronze II] Title: 나머지, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,22 +1,26 @@
-import java.util.Scanner;
-import java.util.HashSet;
-import java.util.Set;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Set<Integer> remainders = new HashSet<>();
-        
-        // 10개의 숫자를 입력받고 각각 42로 나눈 나머지를 Set에 추가
-        for (int i = 0; i < 10; i++) {
-            int number = scanner.nextInt();
-            int remainder = number % 42;
-            remainders.add(remainder);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        //숫자 10개 입력
+        int[] nums = new int[10];
+        for(int i = 0; i < 10; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
         }
-        
-        // 서로 다른 나머지의 개수를 출력
-        System.out.println(remainders.size());
-        
-        scanner.close();
+
+        //각 수를 42로 나누었을 때의 나머지 기록
+        int[] remain = new int[43];  //나올 수 있는 나머지는 0~42이므로 크기를 42로 설정
+        int cnt = 0;  //서로 다른 나머지의 개수
+        for(int i = 0; i < 10; i++) {
+            int tmp = nums[i]%42;
+
+            if (remain[tmp] == 0) {cnt++;}  //처음 나오는 나머지이면 카운트
+            remain[tmp]++;  //해당 나머지의 베열 원소 증가
+        }
+
+        System.out.println(cnt);
     }
 }
